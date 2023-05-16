c81575e147f8700461857b7c0bcaac8892609dd6
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-16 23:17:05 +0300

    Learn @Wendy-tugz day 4 data mutability

---
+++ bonus/bonus4.py
@@ -0,0 +1,22 @@
+# filenames = ["1.Raw Data.tx", "2.Reports.tx", "3.Presentation.txt"]
+#
+# for filename in filenames:
+#     filename = filename.replace('.', '-', 1)
+#     print(filename)
+#
+
+# rate = 0.95
+# dollars = float(input("How many dollars do you have?: "))
+# euros = dollars * rate
+# print("Your amount in euros is: ")
+# print(euros)
+
+# ranking = ['John', 'Sen', 'Lisa']
+# rank = int(input("Enter a rank number: "))-1
+# name = ranking[rank]
+# print(name)
+
+ranking = ['John', 'Sen', 'Lisa']
+name = input("Enter name: ")
+rank = ranking.index(name) + 1
+print(rank)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ todos = []
 
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
@@ -12,8 +12,14 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("Number of item to edit: "))
+            number = number - 1
+            new_todo = input("Enter new to do: ")
+            todos[number] = new_todo
         case 'exit':
             break
+
         # case _:
         #     print("You entered an unknown command.")
 print("bye")
