9d6b14def8c4e582344688f7d90c3f2c88f6a9b7
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-17 12:47:04 +0300

    Learn @Wendy-tugz day 5 enumerate

---
+++ bonus/bonus5.py
@@ -0,0 +1,41 @@
+# waiting_list = ['sean', 'ben', 'john']
+# waiting_list.sort()
+#
+# for index, item in enumerate(waiting_list):
+#     row = f"{index + 1}.{item.capitalize()}"
+#     print (row)
+
+# for i, j in enumerate("abcd"):
+#     phrase = f"Printing {i * 5}"
+#     print(phrase)
+
+
+# filenames = ['document', 'report', 'presentation']
+#
+# for index, item in enumerate(filenames):
+#     row = f"{index}-{item.capitalize()}.txt"
+#     print(row)
+
+# ips = ['100.122.133.105', '100.122.133.111']
+#
+# for i, item in enumerate(ips):
+#     ip = int(input("Enter the index of the  IP you want: "))
+#     message = f"You chose: {ips[ip]}"
+#     print(message)
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# user_choice = int(input("Enter the index of the item: "))
+#
+# message = f"You chose {menu[user_choice]}."
+# print(message)
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# for i, j in enumerate(menu):
+#     print(f"{i}.{j}")
+
+menu = ["pasta", "pizza", "salad"]
+
+for i, j in enumerate(menu):
+    print(f"{i}.{j}")
