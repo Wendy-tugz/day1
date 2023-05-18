7873b512315601d112b7a153c861a71f4cb0297c
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-18 09:05:31 +0300

    Learn @Wendy-tugz day 6 store in python

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-todos = []
-
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
-            todos.append(todo) #todos is updated at this point
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+
+            todos.append(todo)                             #todos is updated at this point
+
             file = open('todos.txt', 'w')
             file.writelines(todos)
         case 'show' | 'display':
