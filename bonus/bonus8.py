ce0db318b8b1659bb9f1d3dceded6d961365664c
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-19 11:33:07 +0300

    Learn @Wendy-tugz day 8 working with files

--- bonus/bonus8.py
+++ bonus/bonus8.py
@@ -1,9 +1,29 @@
-date = input("Enter today's date: ")
-mood = input("How do you rate your mood from 1 to 10: ")
-thoughts = input("Let your thoughts flow:\n ")
+# date = input("Enter today's date: ")
+# mood = input("How do you rate your mood from 1 to 10: ")
+# thoughts = input("Let your thoughts flow:\n ")
+#
+#
+# with open(f"../journal/{date}.txt", "w") as file:
+#     file.write(mood + '\n')
+#     file.write(thoughts)
 
 
-with open(f"../journal/{date}.txt", "w") as file:
-    file.write(mood + '\n')
-    file.write(thoughts)
+
+
+
+while True:
+    with open("../files/sides.txt", 'r') as file:
+        sides = file.readlines()
+
+    side = input("Throw the coin and enter head or tail here: ") + "\n"
+
+    sides.append(side)
+
+    with open("../files/sides.txt", 'w') as file:
+        file.writelines(sides)
+
+    p_heads = sides.count("head\n")
+    percentage = p_heads / len(sides) * 100
+
+    print(f"Heads: {percentage}%")
 
