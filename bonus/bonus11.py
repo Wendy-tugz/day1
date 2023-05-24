8d9246d2e4d479faaccd9d8ce85d306e921fd40b
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-24 15:56:40 +0300

    Learn @Wendy-tugz day 10 custom functions

--- bonus/bonus11.py
+++ bonus/bonus11.py
@@ -21,16 +21,26 @@
 # print(get_average())
 
 
-def get_maxvalue():
-    grades = [9.6, 9.2, 9.7]
+# def get_maxvalue():
+#     grades = [9.6, 9.2, 9.7]
+#
+#     maxvalue = max(grades)
+#     minvalue = min(grades)
+#     message = f"Max: {maxvalue}, Min: {minvalue}"
+#     return message
+#
+#
+# print(get_maxvalue())
 
-    maxvalue = max(grades)
-    minvalue = min(grades)
-    message = f"Max: {maxvalue}, Min: {minvalue}"
-    return message
+def get_maximum():
+    celsius = [14, 15.1, 12.3]
+    maximum = max(celsius)
+    return maximum
 
 
-print(get_maxvalue())
+celsius = get_maximum()
 
+fahrenheit = celsius * 1.8 + 32
+print(fahrenheit)
 
 
