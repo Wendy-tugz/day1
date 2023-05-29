3ea5a18d40730aeb0ac2187a1c56644265a2e286
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-29 12:29:16 +0300

    Learn @Wendy-tugz day 13 decoupling functions

--- bonus/bonus13.py
+++ bonus/bonus13.py
@@ -1,25 +1,54 @@
-feet_inches = input("Enter feet and inches: ")
+# feet_inches = input("Enter feet and inches: ")
+#
+#
+# def parse(feet_inches):
+#     parts = feet_inches.split(" ")
+#     feet = float(parts[0])
+#     inches = float(parts[1])
+#     return {"feet": feet, "inches": inches}
+#
+#
+# def convert(feet, inches):
+#     meters = feet * 0.3048 + inches * 0.0254
+#     return meters
+#
+#
+# parsed = parse(feet_inches)
+#
+# result = convert(parsed['feet'], parsed['inches'])
+#
+# print(f"{parsed['feet']} feet and {parsed['inches']} is equal to {result}")
+#
+# if result < 1:
+#     print("Kid is too small.")
+# else:
+#     print("Kid can use the slide.")
 
 
-def parse(feet_inches):
-    parts = feet_inches.split(" ")
-    feet = float(parts[0])
-    inches = float(parts[1])
-    return {"feet": feet, "inches": inches}
+# def get_age(year_of_birth, current_year=2023):
+#     age = current_year - year_of_birth
+#     return age
+#
+# birthyear = int(input("What is your year of birth? "))
+# age = get_age(birthyear)
+# print(age)
+#
+# if age > 120:
+#     print("Oh men! You're old")
+
+# def get_nr_items(user_input):
+#     items = user_input.split(',')
+#     return len(items)
+#
+#
+# names = input("Enter names separated by commas (no spaces): ")
+# nr_items = get_nr_items(names)
+# print(nr_items)
+
+def calculate_time(h, g=9.80665):
+    t = (2 * h / g) ** 0.5
+    return t
 
 
-def convert(feet, inches):
-    meters = feet * 0.3048 + inches * 0.0254
-    return meters
-
-
-parsed = parse(feet_inches)
-
-result = convert(parsed['feet'], parsed['inches'])
-
-print(f"{parsed['feet']} feet and {parsed['inches']} is equal to {result}")
-
-if result < 1:
-    print("Kid is too small.")
-else:
-    print("Kid can use the slide.")
+time = calculate_time(100)
+print(time)
